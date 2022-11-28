125aec3dfcda077bd6dad25b0f503e19d2f22d6a
Georges-B. Michel <georges@reversense.com>
2022-11-28 16:11:29 +0100

    fix missing __dirname

--- webpack.android.arm64.config.js
+++ webpack.android.arm64.config.js
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as url from "url";
 
 let config = {
     entry: './dist/index.android.arm64.js',
@@ -6,7 +7,7 @@ let config = {
     target: 'node',
     mode: 'production',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(url.fileURLToPath(new URL('.', import.meta.url)), 'dist'),
         filename: 'dxc-agent.android.arm64.min.js',
         library: {
             //name: 'Interruptor',
